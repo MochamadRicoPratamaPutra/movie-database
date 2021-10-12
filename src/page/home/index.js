8078d5d19d19f11fd5c4b66b5420f41bbf470bba
MochamadRicoPratamaPutra <mricopratamaputra@gmail.com>
2021-10-12 17:49:27 +0700

    testing autocomplete searchbox

--- src/page/home/index.js
+++ src/page/home/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Style from './home.module.css'
 import SearchHome from '../../component/base/searchHome'
+import Autocomplete from '../../component/base/autoComplete'
+
 const Home = () => {
   return (
     <div className={Style.container}>
       <h1 className={Style.title}>What movie you want to search?</h1>
+      <Autocomplete suggestions={["Superman", "Batman", "Marvel", "Spiderman", "Iron man"]}/>
       <SearchHome/>
     </div>
   )
